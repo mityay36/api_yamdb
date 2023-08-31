dd3463ef5f746045cf82179f59c164afdde017b4
Sergei Mishin <SMishinMail@gmail.com>
2023-08-31 15:37:58 +0700

    Adds Reviews model Adds Reviews serializer Adds Reviews ViewSet Adds Reviews permissions

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Review, Category, Genre, Title, User
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    title = serializers.HiddenField(
+        default=serializers.PrimaryKeyRelatedField(read_only=True)
+    )
 
     class Meta:
         model = Review
         fields = ('id', 'text', 'author', 'score', 'pub_date', 'title')
+        read_only_fields = ('pub_date',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('title', 'author')
+            )
+        ]
+
+    def validate_score(self, value):
+        if isinstance(value, int) and 1 <= value <= 10:
+            return value
+        raise serializers.ValidationError(
+            'score - should be integer from 1 to 10'
+        )
 
 
 class UserSerializer(serializers.ModelSerializer):
